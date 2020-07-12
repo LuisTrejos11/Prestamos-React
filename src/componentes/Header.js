f52e16b1294815b6be073a0ecf9db38991308d2c
LuisTrejos11 <54832126+LuisTrejos11@users.noreply.github.com>
2020-07-12 12:06:08 -0600

    version 1

---
+++ src/componentes/Header.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+function Header(props){
+
+    
+
+    return(
+        <Fragment>
+            <h1>{props.titulo}</h1>
+            
+        </Fragment>
+             
+     
+    );
+}
+
+export default Header; 
